bf4f335d119e2513db9b03d0b4181ef0be7ed5e3
ChanghaoShih <shichanghao96@gmail.com>
2023-02-06 21:36:13 -0500

    Clean-up nlb code

--- nlb/main_nlb.m
+++ nlb/main_nlb.m
@@ -1,8 +1,9 @@
 %% Load the graph and signals
 clear;close all
+addpath("../GLEN");
 load('area2_bump_train_spikes_resampled.mat');
 load('area2_bump_trial_info.mat');
-%%
+%% 65 neurons
 Acell = zeros(length(spikes),2080);
 Ocell = zeros(length(spikes),65);
 Ycell = zeros(size(spikes));
@@ -20,117 +21,39 @@ param.N = size(X_noisy,1);
 param.T = size(X_noisy,2);
 param.max_iter = 50;
 
-alpha = 10.^[-1:-0.5:-3];
-% beta = 10.^[0:-0.1:-2];
-beta = 10.^[-1.5:-0.5:-3.5];
-gamma = 10.^[-1:-0.5:-3];
-% 
-% alpha = 10^(-1.5);
-% beta = 10^(-2);
-% gamma = 10^(-1);
-
-alpha = 10.^[-1.1:-0.2:-1.9];
-beta = 10.^[-1.6:-0.2:-2.4];
-gamma = 10.^[-1:-0.5:-3];
-
-% alpha = 10.^[-0.6:-0.2:-1.4];
-% beta = 10.^[-1:-0.2:-2];
-
-% alpha = 0.04;
-% beta = 0.01;
-% gamma = 0.01;
-gamma = 0;
+% alpha = 10.^[-1.1:-0.2:-1.9];
+% beta = 10.^[-1.6:-0.2:-2.4];
+% gamma = 10.^[-1:-0.5:-3];
+alpha = 0.04;
+beta = 0.01;
+gamma = 0.1;
 
 %% main loop
 
-for i = 2%1:length(alpha)%2%
-    for j = 1%1:length(beta)%1%
-        for k = 1%1:length(gamma)
+for i = 1:length(alpha)
+    for j = 1:length(beta)
+        for k = 1:length(gamma)
             param.reg_type = 'cgl';
             param.alpha = alpha(i);
             param.beta = beta(j);
-            param.gamma = gamma(k);%*2;
-%             param.trace = trace(k);
+            param.gamma = gamma(k);
 
 
-            [L,Y,offset,L_iter,O_iter] = gl_tv_poisson_log(X_noisy, param);
+            [L,Y,offset,L_iter,O_iter] = glen_tv_poisson(X_noisy, param);
             if any(isnan(L), 'all')
                 convergence(t) = 0;
                 continue
             end
-%             A = -L+diag(diag(L));
-%             imagesc(A);
-%             num_of_edge = sum(A>1E-4,'all')/2;
-%             title(num2str(num_of_edge));
-
-%             Lcell{i,j} = L;
-%             Ocell{i,j} = offset;
-%             Ycell{i,j} = Y;
             
 
             A = -L+diag(diag(L));
             A_full = zeros(65);
             A_full(active_nodes,active_nodes) = A;
             Acell(t,:) = squareform(A_full);
-
             Ycell(t,:,active_nodes) = Y';
             Dcell(t,active_nodes) = diag(L);
-
-            fuck(t,active_nodes) = sum(L,1);
             Ocell(t,active_nodes) = offset;
         end
     end
 end
 end
-
-
-%% clustering
-% clusters = kmeans(Acell,8);
-
-% [coeff,score,latent] = pca(Acell(bump==0,:));
-
-figure(2)
-% mean_firing = squeeze(mean(spikes,2));
-% Ocell(Ocell==0) = -inf;
-% mean_firing = squeeze(mean(exp(Ycell+reshape(Ocell,[length(Ocell),1,size(Ocell,2)])),2));
-mean_firing = exp(Ocell);
-[coeff,score,latent] = pca(mean_firing(bump==0,:));
-
-% for t = 1:length(spikes)
-%     c = cov(squeeze(spikes(t,:,:)));
-%     c = c - diag(diag(c));
-%     Ccell(t,:) = squareform(c);
-% end
-% [coeff,score,latent] = pca(Ccell(bump==0,:));
-% 
-% gscatter(score(:,1),score(:,2),tar(bump==0),parula(8));
-
-scatter3(score(:,1),score(:,2),score(:,3),10,tar(bump==0),'filled')
-% 
-% deg = zeros(length(spikes),65);
-% for t = 1:length(spikes)
-%     deg(t,:) = sum(squareform(Acell(t,:)),1);
-% end
-% [coeff,score,latent] = pca(deg(bump==0,:));
-% gscatter(score(:,1),score(:,2),tar(bump==0),parula(8));
-
-% Lcell_tv_clustering = zeros(length(Lcell_tv_trials),size(spikes,3)^2);
-% for i = 1:length(Lcell_tv_trials)
-%     Lcell_tv_clustering(i,:) = Lcell_tv_trials{i}(:);
-% end
-% clusters = kmeans(Lcell_tv_clustering(bump==0,:),8);
-% nmi = perfeval_clus_nmi(double(clusters),double(tar(bump==0)));
-% [coeff,score,latent] = pca(Lcell_tv_clustering(bump==0,:));
-% % scatter(score(:,1),score(:,2),[],tar(bump==0),'filled');
-% gscatter(score(:,1),score(:,2),tar(bump==0),parula(8));
-% set(gca,'XTick',[]);
-% set(gca,'YTick',[]);
-% print(gcf,'graph_clustering.png','-dpng','-r300');
-% scatter3(score1(:,1),score1(:,2),score1(:,3),[],tar(bump==0));
-% Lcell_clustering = zeros(length(Lcell_trials),size(spikes_smth,3)^2);
-% for i = 1:length(Lcell_trials)
-%     Lcell_clustering(i,:) = Lcell_trials{i}(:);
-% end
-% [coeff,score,latent] = pca(Lcell_clustering(bump==0,:));
-% [coeff,score,latent] = pca(spike_sum(bump==0,:));
-% gscatter(score(:,1),score(:,2),tar(bump==0),parula(8));
--- nlb/utils.m
+++ nlb/utils.m
@@ -1,202 +1,28 @@
-%%
-% Y = Ycell{4,2};
-% L = Lcell{4,2};
-A = -L+diag(diag(L));
-idx = optimalleaforder(linkage(A,'single'),A);
-imagesc(Y(idx,:));
-%% viz
-for i = 1:length(alpha)
-    for j = 1:length(beta)
-        subplot(length(alpha), length(beta), length(beta)*(i-1)+j);
-        L = Lcell{i,j};
-        A = -L+diag(diag(L));
-        imagesc(A);
-%         offset = Ocell{i,j};
-%         offset(offset==0) = -inf;
-%         imagesc(exp(Ycell{i,j}+offset));
-%         num_of_edge = sum(A>1E-4,'all')/2;
-%         title(num2str(num_of_edge));
-    end
-end
-%% viz all
-% tar0 = tar(bump==0);
-% Acell0 = Acell(bump==0,:);
-% [~,idx]=sort(tar0);
-% imagesc(Acell0(idx,:));
-% [~,edge_idx]=sort(mean(Acell0,1),'descend');
-% imagesc(Acell0(idx,edge_idx));
-
-% [~,idx]=sort(0.1*double(bump)+tar);
-% ys = find(tar(idx(1:end-1))~=tar(idx(2:end)));
-% imagesc(Acell(idx,:));
-% yline(ys,'r','LineWidth',1);
-% % [~,edge_idx]=sort(mean(Acell,1),'descend');
-% % imagesc(Acell(idx,edge_idx));
-% xlabel('edge');
-% ylabel('trial');
-% colormap gray
-% 
-% PD = pdist(Acell','hamming');
-% edge_idx = optimalleaforder(linkage(Acell','single'),PD);
-% imagesc(Acell(idx,edge_idx));
-
 %% typical mean firing analysis
 mean_firing = squeeze(mean(spikes,2));
 MdlLinear = fitcdiscr(mean_firing,tar);
 res = predict(MdlLinear,mean_firing);
-acc = sum(res==tar')/length(mean_firing)
-
-%% covariance
-std_firing = squeeze(std(spikes,0,2));
-MdlLinear = fitcdiscr(std_firing,tar);
-res = predict(MdlLinear,std_firing);
-acc = sum(res==tar')/length(std_firing)
-
-%% offset
-Ocell(Ocell==0) = -inf;
-offset_firing = squeeze(mean(exp(Ycell+reshape(Ocell,[length(Ocell),1,size(Ocell,2)])),2));
-MdlLinear = fitcdiscr(offset_firing,tar);
-res = predict(MdlLinear,offset_firing);
-acc = sum(res==tar')/length(offset_firing)
-
-%%
-Ocell(Ocell==0) = -inf;
-offset_firing = exp(Ocell);
-MdlLinear = fitcdiscr(offset_firing,tar);
-res = predict(MdlLinear,offset_firing);
-acc = sum(res==tar')/length(offset_firing)
+acc_mf = sum(res==tar')/length(mean_firing);
 
 %% laplacian
 laplacian = Acell;
 laplacian = laplacian(:,sum(laplacian,1)>0);
 MdlLinear = fitcdiscr(laplacian,tar);
 res = predict(MdlLinear,laplacian);
-acc = sum(res==tar')/length(laplacian)
+acc_laplacian = sum(res==tar')/length(laplacian);
 
 %% degree
 deg = Dcell;
-% deg = double(Dcell>0);
-% deg = deg ./ sum(deg,2);
 deg = deg(:,sum(deg,1)>0);
 MdlLinear = fitcdiscr(deg,tar');
 res = predict(MdlLinear,deg);
-acc = sum(res==tar')/length(deg)
+acc_deg = sum(res==tar')/length(deg);
 
-%%
-deg = Dcell;
-mean_firing = squeeze(mean(spikes,2));
-% X = [deg,mean_firing];
-X = deg;
-% deg = double(Dcell>0);
-% deg = deg ./ sum(deg,2);
-X = X(:,sum(X,1)>0);
-MdlLinear = fitcdiscr(X,tar');
-res = predict(MdlLinear,X);
-acc = sum(res==tar')/length(X)
-
-%%
-N = size(spikes,1);
-idx = randperm(N);
-section = round(N*0.05);
-acc = 0;
-deg = Dcell;
-mean_firing = squeeze(mean(spikes,2));
-std_firing = squeeze(std(spikes,0,2));
-% mean_firing = squeeze(mean(spikes,2));
-% X = [deg,mean_firing];
-X = deg;
-for i = 1:20
-    train_idx = [idx(1:(i-1)*section),idx(i*section+1:end)];
-    test_idx = idx((i-1)*section+1:i*section);
-    X = X(:,sum(X,1)>0);
-    MdlLinear = fitcdiscr(X(train_idx,:),tar(train_idx)');
-    res = predict(MdlLinear,X(test_idx,:));
-    acc = acc + sum(res==tar(test_idx)')/length(test_idx)/20;
-end
-acc
-
-%%
-N = size(spikes,1);
-idx = randperm(N);
-section = round(N*0.1);
-acc = 0;
-mean_firing = squeeze(mean(spikes,2));
-std_firing = squeeze(std(spikes,0,2));
-X = [mean_firing,std_firing];
-for i = 1:10
-    train_idx = [idx(1:(i-1)*section),idx(i*section+1:end)];
-    test_idx = idx((i-1)*section+1:i*section);
-    X = X(:,sum(X,1)>0);
-    MdlLinear = fitcdiscr(X(train_idx,:),tar(train_idx)');
-    res = predict(MdlLinear,X(test_idx,:));
-    acc = acc + sum(res==tar(test_idx)')/length(test_idx)/10;
-end
-acc
-
-%%
-mean_firing = squeeze(mean(spikes,2));
-std_firing = squeeze(std(spikes,0,2));
-X = [mean_firing,std_firing];
-% deg = double(Dcell>0);
-% deg = deg ./ sum(deg,2);
-X = X(:,sum(X,1)>0);
-MdlLinear = fitcdiscr(X(train_idx,:),tar(train_idx)');
-res = predict(MdlLinear,X(test_idx,:));
-acc = sum(res==tar(test_idx)')/length(test_idx)
-
-%% laplacian to degree
-deg = zeros(size(Acell,1),size(spikes,3));
-for i = 1:size(Acell, 1)
-    deg(i,:) = sum(squareform(Acell(i,:)),1);
-end
-deg = deg(:,sum(deg,1)>0);
-MdlLinear = fitcdiscr(deg,tar);
-res = predict(MdlLinear,deg);
-acc = sum(res==tar')/length(deg)
-
-%%
-mean_spike_nobump = squeeze(mean(spikes(bump==0,:,:),1))';
-PD = pdist(mean_spike_nobump);
-idx = optimalleaforder(linkage(PD,'single'),PD);
-imagesc(mean_spike_nobump(idx,:));
-
-%%
+%% plot denoised firing rate
+Ocell(Ocell==0) = -inf;
 for i = 0:7
-mean_spike_bump = squeeze(mean(spikes(bump==1,:,:),1))';
-% PD = pdist(mean_spike_bump);
-% idx = optimalleaforder(linkage(PD,'single'),PD);
-imagesc(mean_spike_bump(idx,:));
-colormap bone
-grid off
-xlabel('time/s');ylabel('neuron')
-yticks([]);ax=gca;
-ax.XAxis.FontSize = 24;
-ax.YAxis.FontSize = 24;
-exportgraphics(ax,strcat('./figures/mean_spike_tar',num2str(i*45),'_bump3.jpg'),'Resolution',300)
-end
-
-%%
-rates = Ycell;
-rates(Ycell==0) = -0.5;
-% imagesc(squeeze(mean(rates(bump==0,:,:),1))');
-mean_rate_bump = squeeze(mean(rates(bump==1,:,:),1))';
-PD = pdist(mean_rate_bump);
-idx = optimalleaforder(linkage(PD,'single'),PD);
-imagesc(mean_rate_bump(idx,:));
-
-mean_spike_0 = squeeze(mean(spikes(mask,:,:),1))';
-PD = pdist(mean_spike_0);
-% idx = optimalleaforder(linkage(PD,'single'),PD);
-imagesc(mean_spike_0(idx,:));
-colormap bone
-%%
-figure(2)
-for i = 0:7
-    Ocell(Ocell==0) = -inf;
     denoised = exp(Ycell+reshape(Ocell,[length(Ocell),1,size(Ocell,2)]));
     mask = (bump==1)&(tar==i*45);
-    % imagesc(squeeze(mean(denoised(bump==0,:,:),1))');
-    % imagesc(squeeze(mean(Ycell(bump==1,:,:),1))');
     mean_denoise_bump = squeeze(mean(denoised(mask,:,:),1))';
     PD = pdist(mean_denoise_bump);
     idx = optimalleaforder(linkage(PD,'single'),PD);
@@ -221,40 +47,3 @@ for i = 0:7
     exportgraphics(ax,strcat('./figures/mean_spike_tar',num2str(i*45),'_bump3.jpg'),'Resolution',300)
 
 end
-
-%% lfads
-% Identify the datasets you'll be using
-% Here we'll add one at ~/lorenz_example/datasets/dataset001.mat
-dc = LorenzExperiment.DatasetCollection('~/lorenz_example/datasets');
-dc.name = 'lorenz_example';
-ds = LorenzExperiment.Dataset(dc, 'dataset001.mat'); % adds this dataset to the collection
-dc.loadInfo; % loads dataset metadata
-
-%%
-% Run a single model for each dataset, and one stitched run with all datasets
-runRoot = '~/lorenz_example/runs';
-rc = LorenzExperiment.RunCollection(runRoot, 'example', dc);
-
-% run files will live at ~/lorenz_example/runs/example/
-
-% Setup hyperparameters, 4 sets with number of factors swept through 2,4,6,8
-par = LorenzExperiment.RunParams;
-par.spikeBinMs = 2; % rebin the data at 2 ms
-par.c_co_dim = 0; % no controller outputs --> no inputs to generator
-par.c_batch_size = 150; % must be < 1/5 of the min trial count
-par.c_gen_dim = 64; % number of units in generator RNN
-par.c_ic_enc_dim = 64; % number of units in encoder RNN
-par.c_learning_rate_stop = 1e-3; % we can stop really early for the demo
-parSet = par.generateSweep('c_factors_dim', [2 4 6 8]);
-rc.addParams(parSet);
-
-% Setup which datasets are included in each run, here just the one
-runName = dc.datasets(1).getSingleRunName(); % == 'single_dataset001'
-rc.addRunSpec(LorenzExperiment.RunSpec(runName, dc, 1));
-
-% Generate files needed for LFADS input on disk
-rc.prepareForLFADS();
-
-% Write a python script that will train all of the LFADS runs using a
-% load-balancer against the available CPUs and GPUs
-rc.writeShellScriptRunQueue('display', 0, 'virtualenv', 'tensorflow');
